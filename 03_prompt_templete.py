2c4dff4f01be7ec74590b642289a8137e2a27b28
Bijen-Lama <158089238+Bijen-Lama@users.noreply.github.com>
2026-07-10 14:55:10 +0545

    03 files

---
+++ 03_prompt_templete.py
@@ -0,0 +1,36 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from configure import settings
+
+# Configuration
+MODEL_NAME = "llama-3.3-70b-versatile"
+
+# Initialize LLM
+llm = ChatGroq(
+    model=MODEL_NAME,
+    api_key=settings.GROQ_API_KEY.get_secret_vlaue()
+)
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are expert {subject} teacher. "
+            "Explain concepts in a simple and begineer friendly way."
+        ),
+        (
+            "human",
+            "{question}"
+        ),
+    ]
+)
+
+# Chain
+chain = prompt | llm | StrOutputParser()
+
+print("Prompt Template Demo")
+
+subject =
